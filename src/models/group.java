733348018f3c22841576f3696a051150b02f6ce7
Anshika Pathak <anshikapathak820@gmail.com>
2026-03-29 22:21:18 +0530

    Create Group model
    Implemented Group class to manage users and expenses, including methods to add users, add expenses, and calculate balances.

--- src/models/group.java
+++ src/models/group.java
@@ -1,1 +1,51 @@
+package models;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Group {
+    private List<User> users;
+    private List<Expense> expenses;
+
+    public Group() {
+        this.users = new ArrayList<>();
+        this.expenses = new ArrayList<>();
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void addExpense(Expense expense) {
+        expenses.add(expense);
+
+        // update balances for each split
+        for (Split split : expense.getSplits()) {
+            split.getUser().updateBalance(-split.getAmount());
+        }
+
+        // payer gets positive amount (people owe them)
+        expense.getPaidBy().updateBalance(expense.getAmount());
+    }
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    // returns final balances for all users
+    public Map<String, Double> getBalances() {
+        Map<String, Double> balances = new HashMap<>();
+
+        for (User user : users) {
+            balances.put(user.getName(), user.getBalance());
+        }
+
+        return balances;
+    }
+}
