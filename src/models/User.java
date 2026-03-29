ffe4f16c32bed43461eab2303c19795aa2286fff
Anshika Pathak <anshikapathak820@gmail.com>
2026-03-29 22:12:03 +0530

    Add User model

--- src/models/User.java
+++ src/models/User.java
@@ -1,1 +1,28 @@
+package models;
 
+public class User {
+    private String name;
+    private double balance;
+
+    public User(String name) {
+        this.name = name;
+        this.balance = 0.0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void updateBalance(double amount) {
+        this.balance += amount;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Balance: " + balance + ")";
+    }
+}
